dc1e4f6fb9190bd8ec47755088a7af78935d5fbf
charles-alex8888 <charles.alex8888@gmail.com>
2021-04-12 10:15:33 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ginblog
+
+# 
+> go mod init ginblog
+# 安装gin框架
+~~~ bash
+go env -w GO111MODULE="auto"
+go get -u github.com/gin-gonic/gin
+go mod tidy
+~~~ 
+
+# ini配置文件包
+> go get gopkg.in/ini.v1
+# gorm包
+> go get -u github.com/jinzhu/gorm
+
+# 目录框架
+- config
+- model
+- api/v1
+- middleware
+- routes
+- utils
+- upload
+- web
---
+++ utils/settings.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"fmt"
+
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode    string
+	HttpPort   string
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassWord string
+	DbName     string
+)
+
+func init() {
+	file, err := ini.Load("config/config.ini")
+	if err != nil {
+		fmt.Println("配置文件读取错误！", err)
+	}
+	LoadServer(file)
+	LoadData(file)
+}
+
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("Server").Key("Appmode").MustString("debug")
+	HttpPort = file.Section("Server").Key("HttpPort").MustString(":3000")
+	fmt.Println(HttpPort)
+}
+
+func LoadData(file *ini.File) {
+	Db = file.Section("database").Key("Db").MustString("debug")
+	DbHost = file.Section("database").Key("DbHost").MustString("localhost")
+	DbPort = file.Section("database").Key("DbPort").MustString("3306")
+	DbUser = file.Section("database").Key("DbUser").MustString("ginblog")
+	DbPassWord = file.Section("database").Key("DbPassWord").MustString("ginblog123")
+	DbName = file.Section("database").Key("DbName").MustString("ginblog")
+}
